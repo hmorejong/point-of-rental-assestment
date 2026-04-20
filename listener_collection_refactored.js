545d0655ef3efd672d148d59a752acca1f531ea2
Hanni Morejon <fullstack@hanni.io>
2026-04-19 20:48:58 -0400

    Initial commit: added assessment files as provided
    Add unmodified source files from developer assessment PDF.

---
+++ listener_collection_refactored.js
@@ -0,0 +1,44 @@
+function ListenerCollection(listeners)
+{
+    this.listeners = listeners;
+}
+Object.assign(ListenerCollection.prototype, {
+    addListener: function(listener)
+    {
+        if(!angular.isFunction(listener)) return angular.noop;
+        if(this.listeners.includes(listener))
+        {
+            this.listeners.push(listener);
+        }
+        var self = this;
+        return function(){
+            self.removeListener(listener);
+        };
+    },
+    removeListener: function(listener)
+    {
+        var index = this.listeners.indexOf(listener);
+        if(index > 0)
+        {
+            this.listeners.splice(index, 1);
+        }
+    },
+    removeAllListeners: function()
+    {
+        this.listeners.length = 0;
+    },
+    hasListeners: function()
+    {
+        return !!this.listeners.length;
+    },
+    trigger: function()
+    {
+        var args = Array.prototype.slice.call(arguments);
+        var promises = [];
+        for(var i = 0; i < this.listeners.length; i++)
+        {
+            promises.push(this.listeners[i].apply(this.listeners[i], args));
+        }
+        return Promise.all(promises);
+    }
+});
